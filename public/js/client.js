3ae0a9709e2a8ee2d992597b26552112bf3f2933
stanley zheng <dev.stanzheng@gmail.com>
2014-07-21 13:13:06 -0400

    corrected phone number tag and send ajax call

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+$( "div" ).click(function() {
+  //alert( "Alerting:" );
+  //console.log($(this).html());
+  var number = $(this).find(".phone").text();
+  text(number);
+});
+
+
+var text = function(number){
+  //use jquery to send the request to text number
+  $.ajax({
+    type: 'POST',
+  //  dataType: 'jsonp',
+    url: 'http://localhost:3000/text',
+    data: {}
+  })
+
+  .done(
+    function (json) {
+      console.log(json);
+  });
+};
