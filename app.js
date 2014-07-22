0ce9cffe62b6921483de52e3d6157e5613e02986
stanley zheng <dev.stanzheng@gmail.com>
2014-07-21 20:24:11 -0400

    saving process

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ app.set('view engine', 'ejs');
 app.locals.title = "xTuple REST To Do App";
 app.set("views", path.join( __dirname, "views" ));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+//remove warning
+
 
 
 app.get('/', function (req, res){
